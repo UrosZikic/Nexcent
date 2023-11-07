58df102915ae23ad585b08a0f90c81719a0679b4
UrosZikic <uroszikic1512988@gmail.com>
2023-11-07 22:05:36 +0100

    navigation update - commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,14 @@
 const openMenu = document.querySelector(".open-menu");
 const closeMenu = document.querySelector(".close-menu");
 const responsiveMenu = document.querySelector(".responsive-menu");
+const body = document.body;
 openMenu.addEventListener("click", () => {
   responsiveMenu.style.display = "block";
+
+  body.style.overflow = "hidden";
 });
 
 closeMenu.addEventListener("click", () => {
   responsiveMenu.style.display = "none";
+  body.style.overflow = "auto";
 });
